c54bbe1edbdf6c68a51fcc8d458b9ac214fbd9a8
karina-talibzhanova <karina.talibzhanova@durham.ac.uk>
2019-01-25 18:12:52 +0000

    q6 and q7 complete

--- Error Correcting Codes/errorcorrectingsummative.py
+++ Error Correcting Codes/errorcorrectingsummative.py
@@ -10,4 +10,17 @@
 
 # question 6
 
+def repetitionEncoder(m, n):
+    return m*n
+
 # question 7
+
+def repetitionDecoder(v):
+
+    if v.count(1) > len(v)//2:
+        return [1]
+    elif v.count(0) > len(v)//2:
+        return [0]
+    else:
+        return []
+
