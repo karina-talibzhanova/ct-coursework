1d775c7522bcd856bc7d4f1460bd5a0cb5201061
karina-talibzhanova <karina.talibzhanova@durham.ac.uk>
2019-01-31 21:04:34 +0000

    q2 complete (so damn hacky lol)

--- Error Correcting Codes/errorcorrectingsummative.py
+++ Error Correcting Codes/errorcorrectingsummative.py
@@ -1,6 +1,81 @@
+import numpy as np
+
+# function HammingG
+# input: a number r
+# output: G, the generator matrix of the (2^r-1,2^r-r-1) Hamming code
+
+def hammingGeneratorMatrix(r):
+    n = 2 ** r - 1
+
+    # construct permutation pi
+    pi = []
+    for i in range(r):
+        pi.append(2 ** (r - i - 1))
+    for j in range(1, r):
+        for k in range(2 ** j + 1, 2 ** (j + 1)):
+            pi.append(k)
+
+    # construct rho = pi^(-1)
+    rho = []
+    for i in range(n):
+        rho.append(pi.index(i + 1))
+
+    # construct H'
+    H = []
+    for i in range(r, n):
+        H.append(decimalToVector(pi[i], r))
+
+    # construct G'
+    GG = [list(i) for i in zip(*H)]
+    for i in range(n - r):
+        GG.append(decimalToVector(2 ** (n - r - i - 1), n - r))
+
+    # apply rho to get Gtranpose
+    G = []
+    for i in range(n):
+        G.append(GG[rho[i]])
+
+    # transpose
+    G = [list(i) for i in zip(*G)]
+
+    return G
+
+
+
+# function decimalToVector
+# input: numbers n and r (0 <= n<2**r)
+# output: a string v of r bits representing n
+def decimalToVector(n, r):
+    v = []
+    for s in range(r):
+        v.insert(0, n % 2)
+        n //= 2
+    return v
+
+
 # question 1
 
 # question 2
+
+def hammingEncoder(m):
+    r = 2
+    while True:
+        if 2**r - r - 1 == len(m):
+            break
+        elif r > len(m):
+            return []
+        r += 1
+
+    generator = hammingGeneratorMatrix(r)
+
+    encoded = np.matmul(m, generator)
+
+    encoded_ls = []
+
+    for i in encoded:
+        encoded_ls.append(i % 2)
+
+    return encoded_ls
 
 # question 3
 
@@ -12,6 +87,7 @@
 
 def repetitionEncoder(m, n):
     return m*n
+
 
 # question 7
 
