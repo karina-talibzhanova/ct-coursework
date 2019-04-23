23baab6a8f30de5eefe15b12950ca884cf1b004b
karina-talibzhanova <karina.talibzhanova@durham.ac.uk>
2019-04-23 13:57:07 +0100

    can find min and delete rows and columns. need to do the actual calculation bit

--- Bioinformatics/objective3/objective3.py
+++ Bioinformatics/objective3/objective3.py
@@ -14,11 +14,8 @@ import networkx as nx
 # and then just concatenate the headings (and obviously do all the appropriate calculations)
 
 
-def WPGMA(file_name):
-	matrix = np.genfromtxt(file_name, dtype=None, encoding="utf-8")  # this is an ndarray
+def find_minimum(matrix):
 	minimum = matrix[1][2]  # set the minimum as the first non-null value in the matrix
-	print(matrix)
-
 	start_index = 2
 
 	for row in range(1, len(matrix)):
@@ -27,7 +24,34 @@ def WPGMA(file_name):
 				minimum = matrix[row][column]
 		start_index += 1
 
-	print(minimum)
+	return minimum
+
+
+def WPGMA(file_name):
+	matrix = np.genfromtxt(file_name, dtype=None, encoding="utf-8")  # this is an ndarray
+	print(matrix)
+
+	while len(matrix) > 3:
+		# find the minimum val in the matrix
+		# reduce the matrix by squishing the row and column together and recalculating distances
+		# print out the reduced matrix
+
+		# use np.delete for deleting rows and columns. good stuff
+
+		minimum = find_minimum(matrix)
+		position = np.where(matrix == minimum)  # finds the position(s) of the min value in the matrix
+
+		# position[0] --> first (x,y) coords --> x = position[0][0], y = position[0][1]
+
+		# deletes the rows and columns of the selected species
+		# max and min so that the correct rows/cols are deleted as the matrix indices will change
+		# 0 denotes a row being deleted, 1 denotes a column being deleted
+		matrix = np.delete(matrix, max(position[0]), 0)
+		matrix = np.delete(matrix, max(position[0]), 1)
+		matrix = np.delete(matrix, min(position[0]), 0)
+		matrix = np.delete(matrix, min(position[0]), 1)
+
+		print(matrix)
 
 	return
 
