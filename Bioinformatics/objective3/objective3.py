36a80decf4638bd00c682000ba509f1db2e9b34a
karina-talibzhanova <karina.talibzhanova@durham.ac.uk>
2019-05-09 16:07:22 +0100

    correctly calculates all the matrices and stuff. next is to plot the graph

--- Bioinformatics/objective3/objective3.py
+++ Bioinformatics/objective3/objective3.py
@@ -13,6 +13,9 @@ import networkx as nx
 # column index --> species along side
 # and then just concatenate the headings (and obviously do all the appropriate calculations)
 
+# calculation: (distance(a, c) + distance(b, c))/2
+# could store all values in an array and add to matrix afterwards
+
 
 def find_minimum(matrix):
 	minimum = matrix[1][2]  # set the minimum as the first non-null value in the matrix
@@ -42,6 +45,18 @@ def WPGMA(file_name):
 		position = np.where(matrix == minimum)  # finds the position(s) of the min value in the matrix
 
 		# position[0] --> first (x,y) coords --> x = position[0][0], y = position[0][1]
+		header = matrix[0][position[0][0]] + matrix[0][position[0][1]]
+
+		array = []
+		for i in range(len(matrix)-1):
+			if matrix[position[0][0]][i+1] == '0':
+				continue
+			elif i+1 == position[0][1]:
+				continue
+			else:
+				distance = (float(matrix[position[0][0]][i+1]) + float(matrix[position[0][1]][i+1]))/2
+				array.append(str(distance))
+		array.insert(0, header)
 
 		# deletes the rows and columns of the selected species
 		# max and min so that the correct rows/cols are deleted as the matrix indices will change
@@ -50,6 +65,16 @@ def WPGMA(file_name):
 		matrix = np.delete(matrix, max(position[0]), 1)
 		matrix = np.delete(matrix, min(position[0]), 0)
 		matrix = np.delete(matrix, min(position[0]), 1)
+
+		# adds new row and column of zeroes to end of matrix
+		matrix = np.append(matrix, [array], axis=0)
+		array.append('0')
+		array = np.transpose([array])
+		matrix = np.append(matrix, array, axis=1)
+
+		# labels the new species
+		matrix[len(matrix)-1][0] = header
+		matrix[0][len(matrix)-1] = header
 
 		print(matrix)
 
